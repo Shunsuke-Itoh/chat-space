258ac109c0b8f83aae3d67c5a00501f8daa63f84
Shunsuke-Itoh <shunsuke.ito.1019@gmail.com>
2019-11-21 14:15:29 +0900

    create_controller&model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :massages, only: [:index]
+    resources :massages, only: [:index, :create]
   end
 end
