187be1676716dae63660c2b38ea969d11c4e14dc
Shunsuke-Itoh <shunsuke.ito.1019@gmail.com>
2019-11-21 12:39:45 +0900

    mount_group_create_function_and_edit_side-bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "groups#index"
 
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :massages, only: [:index]
+  end
 end
